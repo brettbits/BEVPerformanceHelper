ae5bbd1de6ef6ec299c5dd42b19db128582b6b87
Brett Neely (bitsevolving) <brett@bitsevolving.com>
2017-06-21 18:20:02 -0700

    Remove the PerformanceHelper_UnitTesting class (which was only to disable the default file storage behavior) and instead disable file storage with an extension implemented within the PerformanceHelperTests class.

--- Swift/PerformanceHelperTests.swift
+++ Swift/PerformanceHelperTests.swift
@@ -9,11 +9,20 @@ import Foundation
 
 import XCTest
 
+extension PerformanceHelper {
+    class func sharedInstanceWithoutFileStorage() -> PerformanceHelper {
+        let shared = PerformanceHelper.sharedInstance
+        shared.useFileStorage = false
+        return shared
+    }
+}
+
 class PerformanceHelperTests: XCTestCase {
     
+    let ph: PerformanceHelper = PerformanceHelper.sharedInstanceWithoutFileStorage()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
@@ -21,16 +30,8 @@ class PerformanceHelperTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testNotNil() {
+        XCTAssertNotNil(ph)
     }
     
 }
